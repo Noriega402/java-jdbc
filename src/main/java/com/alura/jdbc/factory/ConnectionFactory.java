11114081995fbc8049dde70fb1b224d7abcf2b66
Noriega402 <d.noriegachajon@gmail.com>
2023-05-02 20:08:14 -0600

    agregar un pool de conexiones con c3p0

--- src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -4,21 +4,33 @@
  */
 package com.alura.jdbc.factory;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.sql.DataSource;
 
 /**
  *
  * @author Daniel Noriega
  */
 public class ConnectionFactory {
+    private DataSource datasoruce;
+
+    public ConnectionFactory(){
+        String[] dataConnection = {
+            "jdbc:postgresql://localhost/tienda_alura?useTimeZone=true&serverTimeZone=UTC",
+            "noriega",
+            "server2023$"
+        };
+        var pooledDataSource = new ComboPooledDataSource();
+        pooledDataSource.setJdbcUrl(dataConnection[0]);
+        pooledDataSource.setUser(dataConnection[1]);
+        pooledDataSource.setPassword(dataConnection[2]);
+        this.datasoruce = pooledDataSource;
+    }
+    
     public Connection recuperarConexion() throws SQLException{
-        Connection con = DriverManager.getConnection(
-                "jdbc:postgresql://localhost/tienda_alura?useTimeZone=true&serverTimeZone=UTC",
-                "noriega",
-                "server2023$");
-        
-        return con;
+        return this.datasoruce.getConnection();
     }
 }
